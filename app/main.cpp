6946ca34633c52b4f178c21a76904616768e82eb
Prathinav-kV <prathinav.kv@gmail.com>
2024-10-09 17:54:17 -0400

    Made changes for the app for PID Controller

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,8 @@
-#include "lib.hpp"
-#include "lib1.hpp"
-#include "lib2.hpp"
+#include <iostream>
+#include "pid_controller.hpp"
 
 int main() {
-  dummy();
-  my_function1(30);
-  bool cond = true;
-  if (cond)
-    my_function2(30.3);
+  PID pid(0.1, 0.01, 0.5, 1.0);
+  std::cout << "PID Output: " << pid.calculate(100.0, 90.0) << std::endl;
   return 0;
 }
